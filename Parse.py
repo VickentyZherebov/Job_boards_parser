b19ead40ebba8faf0b9d9ff87e52b9ef09af4c4e
Vickenty Zherebov <vickenty@yandex.ru>
2021-03-12 18:38:27 +0300

    bug_fixed

--- Parse.py
+++ Parse.py
@@ -1,22 +1,22 @@
 from bs4 import BeautifulSoup
 import csv
-# import requests
-#
-# url = 'https://career.habr.com/vacancies/'
-#
-# headers = {
-#     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) '
-#                   'Chrome/88.0.4324.192 Safari/537.36',
-#     'accept': '*/*'}
+import requests
+
+url = 'https://career.habr.com/vacancies/'
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/88.0.4324.192 Safari/537.36',
+    'accept': '*/*'}
 #
 host = 'https://career.habr.com'
 #
-# req = requests.get(url, headers=headers)
-# src = req.text
-# # print(src)
-#
-# with open('index.html', 'w') as file:
-#     file.write(src)
+req = requests.get(url, headers=headers)
+src = req.text
+# print(src)
+
+with open('index.html', 'w') as file:
+    file.write(src)
 
 with open('vacancies.csv', 'w', encoding='utf-8') as file:
     writer = csv.writer(file)
@@ -38,14 +38,14 @@ for item in vacancy_card:
     company_title = item.find('a', class_='link-comp link-comp--appearance-dark').text
     company_link = host + item.find('a', class_='link-comp link-comp--appearance-dark').get('href')
     title_link = host + item.find('a', class_='vacancy-card__title-link').get('href')
-    date = item.find('div', class_='vacancy-card__date').find('time', class_='basic-date').text
+    date = item.find('div', class_='vacancy-card__date').find('time', class_='basic-date').text.strip()
     icon_link = item.find('a', class_='vacancy-card__icon-link').find('img', 'vacancy-card__icon').get('src')
     salary = item.find('div', class_='basic-salary').text
-    # skills = []
-    skills = item.find('div', class_='vacancy-card__skills').find_all('span', 'preserve-line')
-    for skill in skills:
-        skill = skill.find('a', class_='link-comp link-comp--appearance-dark').text
-        skills.append(skill)
+    skills_all = []
+    skill_elements = item.find('div', class_='vacancy-card__skills').find_all('span', 'preserve-line')
+    for skill_element in skill_elements:
+        skill_element = skill_element.find('a', class_='link-comp link-comp--appearance-dark').text
+        skills_all.append(skill_element)
     # vacancy_skills = []
     # for skill in skills:
     #     vacancy_skill = skill.find('href')
@@ -57,7 +57,7 @@ for item in vacancy_card:
     print(date)
     print(icon_link)
     print(salary)
-    # print(skills)
+    print(skills_all)
     # print(vacancy_skills)
     print('______________')
     with open('vacancies.csv', 'a', encoding='utf-8') as file:
