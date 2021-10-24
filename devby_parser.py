4304d4965f5db9a03d3b7851140287e5704516dd
Vickenty Zherebov <vickenty@yandex.ru>
2021-10-24 14:45:53 +0300

    1. Немного причесал код, но все еще впереди

--- devby_parser.py
+++ devby_parser.py
@@ -20,6 +20,7 @@ HEADERS = (
 )
 result = []
 
+
 class DevByParser:
 
     def __init__(self):
@@ -27,11 +28,8 @@ class DevByParser:
         self.session.headers = {'accept': '*/*', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
                                                                ' AppleWebKit/537.36 (KHTML, like Gecko) '
                                                                'Chrome/78.0.3904.108 Safari/537.36',
-                                                                'Accept-Language': 'ru',
-                                                             }
-
-
-
+                                                               'Accept-Language': 'ru'
+                                }
 
     def get_page(self, url, page: int = None):
         params = {}
@@ -46,9 +44,8 @@ class DevByParser:
         r = self.session.get(url, params=params)
         return r.text
 
-
     def get_blocks(self):
-        text = self.get_page(page=2, url = 'https://companies.dev.by/')
+        text = self.get_page(page=2, url='https://companies.dev.by/')
         soup = bs(text, 'lxml')
         container = soup.find('tbody')
         for item in container:
@@ -81,9 +78,8 @@ class DevByParser:
                             'person_name': person_name,
                             'person_position': person_position
                         })
-
                 # self.save_results()
-                    # block = self.parse_company_block(item=item, company_name=company_name, company_page_url=company_page_url)
+                # block = self.parse_company_block(item=item, company_name=company_name, company_page_url=company_page_url)
             except:
                 pass
             # self.save_results()
@@ -106,7 +102,6 @@ class DevByParser:
             # logger.info(f'Получили {len(self.result)} элементов')
             # logger.debug('-' * 100)
             # self.save_results()
-
 
         # text = self.get_page(page=0, url=person_url)
         # pprint.pprint(text)
@@ -133,14 +128,13 @@ class DevByParser:
             writer.writerow(('Компания', 'Ссылка на компанию', 'Ссылка на сотрудника', 'Имя сотрудника', 'Должность',))
             for item in result:
                 print(item)
-                writer.writerow((item['company_name'], item['company_page_url'], item['person_url'], item['person_name'], item['person_position']))
+                writer.writerow((item['company_name'], item['company_page_url'], item['person_url'],
+                                 item['person_name'], item['person_position']))
 
 
 def main():
     p = DevByParser()
     p.get_blocks()
-
-
 
 
 if __name__ == '__main__':
--- hh_parser.py
+++ hh_parser.py
@@ -1,12 +1,21 @@
 import requests
 import csv
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
 headers = {'accept': '*/*',
-           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
-base_url = 'https://spb.hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text=%D0%90%D0%B3%D1%80%D0%BE%D0%BF%D1%80%D0%BE%D0%BC%D1%8B%D1%88%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%B9&page=0'
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/78.0.3904.108 Safari/537.36'}
+base_url = 'https://spb.hh.ru/search/vacancy?' \
+           'clusters=true&' \
+           'enable_snippets=true&' \
+           'salary=&' \
+           'st=searchVacancy&' \
+           'text=Агропромышленный&' \
+           'page=0'
 
 
+# О чем эта функция:
+# 1. на входе принимает урл откуда будет происходить парсинг и заголовки для работы bs4
 def hh_parse(base_url, headers):
     jobs = []
     urls = []
@@ -16,27 +25,34 @@ def hh_parse(base_url, headers):
     request = session.get(base_url, headers=headers)
 
     if request.status_code == 200:
-        soup = bs(request.content, 'lxml')
+        soup = BeautifulSoup(request.content, 'lxml')
         try:
             pagination = soup.find_all('a', attrs={'data-qa': 'pager-page'})
             count = int(pagination[-1].text)
             for i in range(count):
-                url = f'https://spb.hh.ru/search/vacancy?clusters=true&enable_snippets=true&salary=&st=searchVacancy&text=%D0%90%D0%B3%D1%80%D0%BE%D0%BF%D1%80%D0%BE%D0%BC%D1%8B%D1%88%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%B9&page={i}'
+                url = f'https://spb.hh.ru/search/vacancy?' \
+                      f'clusters=true&' \
+                      f'enable_snippets=true&' \
+                      f'salary=&' \
+                      f'st=searchVacancy&' \
+                      f'text=Агропромышленный&' \
+                      f'page={i}'
+                print(url)
                 if url not in urls:
                     urls.append(url)
-
-        except:
-            pass
+        except Exception as e:
+            print(f'{e} Это что ещё такое?')
+    print(f'количество url = {len(urls)}')
 
     for url in urls:
         request = session.get(url, headers=headers)
-        soup = bs(request.content, 'lxml')
-
-
+        soup = BeautifulSoup(request.content, 'lxml')
+        print(soup)
         divs = soup.find_all('div', attrs={'data-qa': 'vacancy-serp__vacancy'})
         for div in divs:
             try:
                 title = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'}).text
+                print(title)
                 href = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-employer'})['href']
                 href_vac = div.find('a', attrs={'data-qa': 'vacancy-serp__vacancy-title'})['href']
                 # try:
@@ -46,17 +62,14 @@ def hh_parse(base_url, headers):
                 #     print(name, tel)
                 # except:
                 #     pass
-                jobs.append({
-                    'title': title,
-                    'href': f'https://spb.hh.ru{href}',
-                    'href_vac': href_vac
-                })
-
-            except:
-                pass
-
-
-
+                jobs.extend(
+                    {'title': title,
+                     'href': f'https://spb.hh.ru{href}',
+                     'href_vac': href_vac
+                     })
+                print(title)
+            except Exception as e:
+                print(f'{e} Это что ещё такое?')
         print(len(jobs))
     else:
         print('Error or Done ' + str(request.status_code))
@@ -68,10 +81,7 @@ def hh_page_parse(jobs, headers):
     session = requests.Session()
     r_page = session.get(page_url, headers=headers)
     if r_page.status_code == 200:
-        soup = bs(r_page.content, 'lxml')
-
-
-
+        soup = BeautifulSoup(r_page.content, 'lxml')
 
 
 def file_writer(jobs):
@@ -81,8 +91,8 @@ def file_writer(jobs):
         for job in jobs:
             a_pen.writerow((job['title'], job['href'], job['href_vac']))
 
-# get data from each page
 
+# get data from each page
 jobs = hh_parse(base_url, headers)
 page_data = hh_page_parse(jobs, headers)
 file_writer(jobs)
